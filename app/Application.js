cfe098ed7dbaa3571a7acdc49cbf18fb347559f3
SHANYUAN <SHANYUAN55@icloud.com>
2015-11-30 11:20:29 +0100

    login works

--- app/Application.js
+++ app/Application.js
@@ -11,9 +11,27 @@ Ext.define('SppAppClassic.Application', {
     stores: [
         // TODO: add global / shared stores here
     ],
+
+    views: [
+        'SppAppClassic.view.login.Login',  // used in launch
+        'SppAppClassic.view.main.Main'  // used in launch
+    ],
     
     launch: function () {
-        // TODO - Launch the application
+        // It's important to note that this type of application could use
+        // any type of storage, i.e., Cookies, LocalStorage, etc.
+        var loggedIn;
+
+        // Check to see the current value of the localStorage key
+        loggedIn = localStorage.getItem("TutorialLoggedIn");
+
+        // This ternary operator determines the value of the TutorialLoggedIn key.
+        // If TutorialLoggedIn isn't true, we display the login window,
+        // otherwise, we display the main view
+        Ext.create({
+            // if loggedIn exists, launch app-main, else launch login
+            xtype: loggedIn ? 'app-main' : 'login'
+        });
     },
 
     onAppUpdate: function () {
--- app/view/main/NavbarController.js
+++
@@ -1,5 +0,0 @@
-Ext.define('SppAppClassic.view.main.NavbarController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.main-navbar'
-    
-});
